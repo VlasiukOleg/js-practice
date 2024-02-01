a234794df6bdc0a84662f5ed6a8e05c52c8009c4
Oleg <ikslprint@gmail.com>
2024-02-01 19:22:19 +0200

    finished practice (promise)

--- js/promise.js
+++ js/promise.js
@@ -1,15 +1,54 @@
-const isSuccess = true;
+// const isSuccess = Math.random() > 0.45;
 
-const promise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    if (isSuccess) {
-      resolve('Success! Value passed to resolve function');
-    } else {
-      reject('Error! Error passed to reject function');
-    }
-  }, 2000);
-});
+// const promise = new Promise((resolve, reject) => {
+//   setTimeout(() => {
+//     if (isSuccess) {
+//       resolve('Success! Value passed to resolve function');
+//     } else {
+//       reject('Error! Error passed to reject function');
+//     }
+//   }, 2000);
+// });
 
-console.log(promise);
+// promise.then(value => console.log(value)).catch(error => console.log(error));
 
-promise.then(value => console.log(value)).catch(error => console.log(error));
+// promise
+//   .then(result => {
+//     console.log(result);
+//     return 5;
+//   })
+//   .then(x => console.log(x))
+//   .finally(console.log('any case'));
+
+// ! Промисификация функций
+
+// const DELAY = 2000;
+
+// const makeDish = dish => {
+//   const isMakeDish = Math.random() > 0.5;
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       if (isMakeDish) {
+//         resolve(`Вот Ваш ${dish}`);
+//       } else {
+//         reject('Упс закончились продукты');
+//       }
+//     }, DELAY);
+//   });
+// };
+
+// makeDish('пирожок').then(onMakeDishSuccess).catch(onMakeDishError);
+
+// function onMakeDishSuccess(result) {
+//   console.log('onMakeDishSuccess');
+//   console.log(result);
+// }
+
+// function onMakeDishError(error) {
+//   console.log(' onMakeDishError');
+//   console.log(error);
+// }
+
+fetch('https://pokeapi.co/api/v2/pokemon')
+  .then(r => r.json())
+  .then(result => console.log(result.results[1]));
